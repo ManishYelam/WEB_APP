a83004302204a3b0a3f84359e21e939fb6d9bad5
ManishYelam <manishyelam@gmail.com>
2024-09-08 16:12:41 +0530

    yes

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,11 @@
 import React from 'react'
-import EmployeeData from './component/EmployeeData'
-import { UserList } from './component/UserList'
-
 
 const App = () => {
   return (
     <div>
-    <EmployeeData />
-    <UserList />
+
     </div>
   )
 }
 
-export default App
+export default App;
